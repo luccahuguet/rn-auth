36632f49aeb4614ee50718fc4eb590167e088670
luccahuguet <lucca.huguet2@gmail.com>
2024-06-03 16:06:28 -0300

    chore: Refactor authentication context and header in App.js

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
+import React, { useContext } from "react";
 
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import { Colors } from "./constants/styles";
-import { AuthContext } from "./store/auth-context";
-import AuthContextProvider from "./store/auth-context";
-import { useContext } from "react";
+import AuthContextProvider, { AuthContext } from "./store/auth-context";
+import IconButton from "./components/ui/IconButton";
 
 const Stack = createNativeStackNavigator();
 
@@ -28,6 +28,7 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const auth = useContext(AuthContext);
   return (
     <Stack.Navigator
       screenOptions={{
@@ -36,7 +37,20 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon="exit"
+              size={24}
+              color={tintColor}
+              onPress={auth.logout}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
--- store/auth-context.js
+++ store/auth-context.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 export const AuthContext = createContext({
   token: null,
   isAuthenticated: false,
-  authenticate: (token) => {},
+  authenticate: () => {},
   logout: () => {},
 });
 
